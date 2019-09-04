3694680d70e0234213d4a552e251f8b5fdd617e3
Ian Trent <ian.m.trent@gmail.com>
2019-09-03 21:11:44 -0400

    Added poster image rendering ability

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { posterURL } from './../config';
 
 const Card = ({ original_title, release_date, vote_average, poster_path }) => {
     return (
         <div className="card">
             <p>{original_title}</p>
+            <img
+                src={`${posterURL}/${poster_path}`}
+                alt={`${original_title} movie poster`}
+                width="100px"
+            />
         </div>
     );
 };
--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const apiKey = process.env.REACT_APP_API_KEY;
 export const apiUrl = process.env.REACT_APP_API_URL;
+export const posterURL = 'http://image.tmdb.org/t/p/original';
