3ab509fa30333cd133e81fb756494a0d16010e23
Ian Trent <ian.m.trent@gmail.com>
2019-09-05 23:39:46 -0400

    Functionality to allow  for pagination to take category argument

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Pagination = ({ page, totalPages }) => {
+const Pagination = ({ page, totalPages, type = 'popular' }) => {
     return (
         <div className="pagination">
             <p>{page}</p>
             <p>{totalPages}</p>
             {page === 1 || page === 0 ? null : (
-                <Link to={page - 1}>Page {page - 1}</Link>
+                <Link to={`/category/${type}?page=${page - 1}`}>
+                    Page {page - 1}
+                </Link>
             )}
             {page === totalPages ? null : (
-                <Link to={`?page=${page + 1}`}>Page {page + 1}</Link>
+                <Link to={`/category/${type}?page=${page + 1}`}>
+                    Page {page + 1}
+                </Link>
             )}
         </div>
     );
