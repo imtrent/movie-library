2cff983664570af7778e3baf25fd341f44aadd66
Ian Trent <ian.m.trent@gmail.com>
2019-09-07 15:49:05 -0400

    Added global styles to router component

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import MovieDashboard from './../pages/MovieDashboard';
-import NotFound from '../pages/NotFound';
+import { GlobalStyles } from './../components/Settings';
+import Dashboard from './../pages/Dashboard';
+import NotFound from './../pages/NotFound';
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
-            <div>
-                <Switch>
-                    <Route path="/" exact={true} component={MovieDashboard} />
-                    <Route
-                        path="/category/:type"
-                        render={props => <MovieDashboard {...props} />}
-                    />
-                    <Route component={NotFound} />
-                </Switch>
-            </div>
+            <GlobalStyles />
+            <Switch>
+                <Route path="/" exact={true} component={Dashboard} />
+                <Route
+                    path="/category/:type"
+                    render={props => <Dashboard {...props} />}
+                />
+                <Route component={NotFound} />
+            </Switch>
         </BrowserRouter>
     );
 };
