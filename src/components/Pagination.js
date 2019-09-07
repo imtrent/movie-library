866c2510feba6924f959b4c8bfdcc4af6e4246bd
Ian Trent <ian.m.trent@gmail.com>
2019-09-06 23:49:25 -0400

    Fixed pagination issue where it concats instead of adds

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Pagination = ({ page, totalPages, type = 'popular' }) => {
+const Pagination = ({ page = '1', totalPages, type = 'popular' }) => {
+    // Turn page string into number
+    const pageNum = page * 1;
+
     return (
         <div className="pagination">
             <p>{page}</p>
             <p>{totalPages}</p>
             {page === 1 || page === 0 ? null : (
-                <Link to={`/category/${type}?page=${page - 1}`}>
-                    Page {page - 1}
+                <Link to={`/category/${type}?page=${pageNum - 1}`}>
+                    Page {pageNum - 1}
                 </Link>
             )}
             {page === totalPages ? null : (
-                <Link to={`/category/${type}?page=${page + 1}`}>
-                    Page {page + 1}
+                <Link to={`/category/${type}?page=${pageNum + 1}`}>
+                    Page {pageNum + 1}
                 </Link>
             )}
         </div>
