355a1f13abf3cc72ed91b1febd7c5eda7ec771e6
Ian Trent <ian.m.trent@gmail.com>
2019-09-10 23:06:05 -0400

    Created axios instance

--- src/config.js
+++ src/config.js
@@ -1,3 +1,9 @@
-export const apiKey = process.env.REACT_APP_API_KEY;
-export const apiUrl = process.env.REACT_APP_API_URL;
+import axios from 'axios';
+
+export const APIKey = process.env.REACT_APP_API_KEY;
+export const APIUrl = process.env.REACT_APP_API_URL;
 export const posterURL = 'http://image.tmdb.org/t/p/original';
+
+export const API = axios.create({
+    baseURL: APIUrl
+});
