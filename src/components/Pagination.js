804407c9efba7d91ee0748c6d095cd0088968c61
Ian Trent <ian.m.trent@gmail.com>
2019-09-12 22:31:39 -0400

    Added functionality for pagination to change query of any url

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Pagination = ({ page, totalPages }) => {
+    const url = window.location.pathname;
+
     return (
         <div className="pagination">
             {page === 1 || page === 0 ? null : (
-                <Link className="button">Page {page - 1}</Link>
+                <Link to={`${url}?page=${page * 1 - 1}`} className="button">
+                    Page {page * 1 - 1}
+                </Link>
             )}
             {page === totalPages ? null : (
-                <Link className="button">Page {page + 1}</Link>
+                <Link to={`${url}?page=${page * 1 + 1}`} className="button">
+                    Page {page * 1 + 1}
+                </Link>
             )}
         </div>
     );
