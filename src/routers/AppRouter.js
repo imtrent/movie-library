b790a502d441eecd83078111075fe38adfb1ca9f
Ian Trent <ian.m.trent@gmail.com>
2019-09-14 23:29:29 -0400

    Changed out navigation for header

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import Navigation from './../components/Navigation';
+import Header from './../components/Header';
 import Browse from './../pages/Browse';
 import Genre from './../pages/Genre';
 import NotFound from './../pages/NotFound';
@@ -9,29 +9,31 @@ import MovieDetails from './../pages/MovieDetails';
 const AppRouter = () => {
     return (
         <BrowserRouter>
-            <Navigation />
-            <Switch>
-                <Route path="/" exact={true}>
-                    <Redirect from="/" to="/browse/popular" />
-                </Route>
-                <Route path="/browse" exact={true}>
-                    <Redirect from="/browse" to="/browse/popular" />
-                </Route>
-                <Route
-                    path="/browse/:type"
-                    exact={true}
-                    render={props => <Browse {...props} />}
-                />
-                <Route
-                    path="/genre/:type"
-                    render={props => <Genre {...props} />}
-                />
-                <Route
-                    path="/movie/:id"
-                    render={props => <MovieDetails {...props} />}
-                />
-                <Route component={NotFound} />
-            </Switch>
+            <div className="container">
+                <Header />
+                <Switch>
+                    <Route path="/" exact={true}>
+                        <Redirect from="/" to="/browse/popular" />
+                    </Route>
+                    <Route path="/browse" exact={true}>
+                        <Redirect from="/browse" to="/browse/popular" />
+                    </Route>
+                    <Route
+                        path="/browse/:type"
+                        exact={true}
+                        render={props => <Browse {...props} />}
+                    />
+                    <Route
+                        path="/genre/:type"
+                        render={props => <Genre {...props} />}
+                    />
+                    <Route
+                        path="/movie/:id"
+                        render={props => <MovieDetails {...props} />}
+                    />
+                    <Route component={NotFound} />
+                </Switch>
+            </div>
         </BrowserRouter>
     );
 };
