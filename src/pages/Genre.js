7f2e5eac63e63bcd12acdc868f6bdb6ad438e791
Ian Trent <ian.m.trent@gmail.com>
2019-09-24 23:26:28 -0400

    Trying to work on a fix for when reloading genre page

--- src/pages/Genre.js
+++ src/pages/Genre.js
@@ -1,24 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
-import { getGenre } from './../api/APIUtils';
+import { getGenres, getGenre } from './../api/APIUtils';
 import MovieList from '../components/MovieList';
 import Pagination from '../components/Pagination';
 
 const Genre = props => {
     const [movies, setMovies] = useState([]);
+    const [genres, setGenres] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
 
     const type = '18';
     const page = queryString.parse(props.location.search).page || 1;
 
-    const loadData = (type, page) => {
-        getGenre(type, page).then(res => {
-            setMovies(res.data);
-        });
+    const loadGenres = () => {
+        getGenres()
+            .then(res => {
+                setGenres(res.data);
+            })
+            .catch(err => {
+                setError(true);
+            });
+    };
+
+    const loadMovies = (type, page) => {
+        getGenre(type, page)
+            .then(res => {
+                setMovies(res.data);
+            })
+            .catch(err => {
+                setError(true);
+            });
     };
 
     useEffect(() => {
-        loadData(type, page);
+        loadGenres();
+        loadMovies(type, page);
     }, [window.location.search, type]);
 
     return (
