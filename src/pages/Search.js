f2f0f7bbfc8946b1287ac4d38392e0063657879d
Ian Trent <ian.m.trent@gmail.com>
2019-10-10 21:37:57 -0400

    Added error placeholder, and fixed query and page handling

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,19 +1,26 @@
 import React, { useState, useEffect } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
 import queryString from 'query-string';
 import { getSearch } from './../api/APIUtils';
 import Loading from './../components/utils/Loading';
 import MovieList from '../components/MovieList';
 import Pagination from '../components/Pagination';
 
-const Search = props => {
+const Search = () => {
     const [movies, setMovies] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
+    const { search } = useParams();
+    const location = useLocation();
 
-    const query = props.match.params.search;
-    const page = queryString.parse(props.location.search).page || 1;
+    /*
+        1. Get the search request parameter
+        2. Parse the page query, use 1 if page query not specified
+    */
+    const query = search;
+    const page = queryString.parse(location.search).page || 1;
 
-    const loadData = (type, page) => {
+    const loadData = (query, page) => {
         getSearch(query, page)
             .then(res => {
                 setMovies(res.data);
@@ -32,19 +39,38 @@ const Search = props => {
         return <Loading />;
     }
 
+    if (error) {
+        return <p>test</p>;
+    }
+
     if (movies.results === undefined || movies.results.length === 0) {
         return (
-            <div class="no-results">
-                <h2>Sorry!</h2>
-                <p>It looks like there were no results found for {query}</p>
+            <div className="wrapper">
+                <div className="container">
+                    <div className="no-results">
+                        <h2>Oh No!</h2>
+                        <p>
+                            It looks like there were no results found for{' '}
+                            <strong>{query}</strong>
+                        </p>
+                        <a className="button" href="/">
+                            Return Home
+                        </a>
+                    </div>
+                </div>
             </div>
         );
     }
 
     return (
-        <div>
-            <MovieList movies={movies} />
-            <Pagination page={movies.page} totalPages={movies.total_pages} />
+        <div className="wrapper">
+            <div className="container">
+                <MovieList movies={movies} />
+                <Pagination
+                    page={movies.page}
+                    totalPages={movies.total_pages}
+                />
+            </div>
         </div>
     );
 };
